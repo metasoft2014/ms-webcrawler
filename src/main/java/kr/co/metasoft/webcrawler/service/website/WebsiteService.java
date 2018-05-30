5d266570df1c3d01d13cba362f5e0f2a8e9d586e
jspark <jspark@meta-soft.co.kr>
2018-05-30 14:58:44 +0900

    update websiteDeleteById cascade

--- src/main/java/kr/co/metasoft/webcrawler/service/website/WebsiteService.java
+++ src/main/java/kr/co/metasoft/webcrawler/service/website/WebsiteService.java
@@ -2,6 +2,7 @@ package kr.co.metasoft.webcrawler.service.website;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,13 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import kr.co.metasoft.webcrawler.entity.crawler.CrawlerEntity;
+import kr.co.metasoft.webcrawler.entity.crawler.CrawlerFieldEntity;
 import kr.co.metasoft.webcrawler.entity.website.WebsiteEntity;
+import kr.co.metasoft.webcrawler.repository.crawler.CrawlerFieldRepository;
+import kr.co.metasoft.webcrawler.repository.crawler.CrawlerRepository;
 import kr.co.metasoft.webcrawler.repository.website.WebsiteRepository;
 
 @Service
@@ -18,6 +24,12 @@ public class WebsiteService {
 
 	@Autowired
 	private WebsiteRepository websiteRepository;
+
+	@Autowired
+	private CrawlerRepository crawlerRepository;
+
+	@Autowired
+	private CrawlerFieldRepository crawlerFieldRepository;
 
 	public Page<WebsiteEntity> websiteSelectList(WebsiteEntity websiteEntity, Pageable pageable) {
 		Example<WebsiteEntity> example = Example.of(websiteEntity);
@@ -41,7 +53,20 @@ public class WebsiteService {
 		return websiteRepository.save(websiteEntity);
 	}
 
+	@Transactional
 	public void websiteDeleteById(Long websiteId) {
+		CrawlerEntity exampleCrawlerEntity = new CrawlerEntity();
+		exampleCrawlerEntity.setWebsiteId(websiteId);
+		List<CrawlerEntity> crawlerEntityList = crawlerRepository.findAll(Example.of(exampleCrawlerEntity));
+		crawlerEntityList.forEach((crawlerEntity) -> {
+			CrawlerFieldEntity exampleCrawlerFieldEntity = new CrawlerFieldEntity();
+			exampleCrawlerFieldEntity.setCrawlerId(crawlerEntity.getCrawlerId());
+			List<CrawlerFieldEntity> crawlerFieldEntityList = crawlerFieldRepository.findAll(Example.of(exampleCrawlerFieldEntity));
+			crawlerFieldEntityList.forEach((crawlerFieldEntity) -> {
+				crawlerFieldRepository.delete(crawlerFieldEntity);
+			});
+			crawlerRepository.delete(crawlerEntity);
+		});
 		websiteRepository.deleteById(websiteId);
 	}
 
